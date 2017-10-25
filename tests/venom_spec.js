9eb1d16e0ceb9d6a29ff6b7cf567b3e358562952
Olwen <o.davies@nhm.ac.uk>
2017-10-25 10:31:16 +0100

    WIP tests

--- tests/venom_spec.js
+++ tests/venom_spec.js
@@ -8,24 +8,62 @@ describe('venom interactive App', function() {
 	// 	// callback(); - should use callback()
 	// });
 
+	beforeEach(function() {
+		browser.get('file:///C:/Projects/nhm-gallery-interactive-venom-papyrus/index.html');
+		browser.driver.sleep(1000);
+			
+	});
+
 
 	it ('should go to landing when checked', function(callback){
-		browser.get('file:///C:/Projects/nhm-gallery-interactive-venom-papyrus/index.html');
 		element(by.id('content')).click();
-		browser.driver.sleep(1000);
 		expect($('.flex-item').isPresent()).toBeTruthy();
 		callback();
 	});
 
-	it('clicking on S&O goes to S&O Url'){
-		browser.actions().
-			mouseDown(element(by.id('#content > flex-container-1'))).
-	// expect URL to be....
-			
-	}
+	it ('should have title Brooklyn Papyrus', function(){
+		element(by.id('content')).click();
+		browser.driver.sleep(1000);
+		expect(element(by.css('h1')).getText()).toEqual('Brooklyn Papyrus');
+	});
 
-	it('touch pad on S&O goes to S&O Url'){
-		browser.touchActions().
-			tap(element(by.id('#content > flex-container-1'))).
-	// expect URL to be....	
-})
+	it ('should go to Symptoms & Outcomes when box is mouse clicked', function(){
+
+	});
+
+	it ('should go to Treatments when box is mouse clicked', function(){
+
+	});
+
+	it ('should stay on landing page when clicking out of boxes', function(){
+
+	});
+
+	it ('should go to Symptoms & Outcomes when box is touched', function(){
+
+	});
+
+	it ('should go to Treatments when box is touched', function(){
+
+	});
+
+	it ('should stay on landing page when touching screen out of boxes', function(){
+
+	});
+
+
+
+});
+
+// 	it ('clicking on S&O goes to S&O Url'){
+// 		browser.actions().
+// 			mouseDown(element(by.id('#content > flex-container-1'))).
+// 	// expect URL to be....
+			
+// 	}
+
+// 	it ('touch pad on S&O goes to S&O Url'){
+// 		browser.touchActions().
+// 			tap(element(by.id('#content > flex-container-1'))).
+// 	// expect URL to be....	
+// }
